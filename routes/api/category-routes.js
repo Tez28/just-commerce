bf4d54c9c9c2b2998c5df6fb39912bd9ffad115e
Tez28 <courtezcannady@gmail.com>
2022-03-20 11:03:15 -0400

    update category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -62,6 +62,22 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData[0]) {
+      res.status(404).json({message: 'No category matching this id found'});
+      return;
+    }
+    res.sjon(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.delete('/:id', (req, res) => {
